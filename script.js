cf66f510c722567abe3a2cf4ca193f0fa97bd421
kazuyai <g2172026@tcu.ac.jp>
2023-12-18 01:42:31 +0900

    〇×の仮表示

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ document.addEventListener('DOMContentLoaded', generateQuizzes);
 function showResults() {
     let currentQuiz = 0;
     const resultsContainer = document.getElementById('results');
+    const headerHeight = document.querySelector('header').offsetHeight;
     resultsContainer.innerHTML = ''; // 既存の結果をクリア
 
     function displayResult() {
@@ -56,17 +57,30 @@ function showResults() {
 
         const userAnswer = document.querySelector(`input[name="radio${currentQuiz + 1}"]:checked`);
         const result = document.createElement('div');
+        let isCorrect = false;
 
         if (userAnswer && userAnswer.id === quizzes[currentQuiz].correct) {
             result.textContent = `${currentQuiz + 1}問目: 正解！`;
+            isCorrect = true;
         } else {
             result.textContent = `${currentQuiz + 1}問目: 不正解`;
         }
 
         resultsContainer.appendChild(result);
 
-        // 次のクイズにスクロール
-        document.getElementsByClassName('quiz-card')[currentQuiz].scrollIntoView({ behavior: 'smooth' });
+        // 選択した選択肢の上に〇または✕を表示
+        if (userAnswer) {
+            const mark = document.createElement('span');
+            mark.textContent = isCorrect ? '〇' : '✕';
+            mark.className = isCorrect ? 'correct-mark' : 'incorrect-mark';
+            userAnswer.parentNode.appendChild(mark);
+        }
+
+        // ヘッダーとビューポートの高さを考慮したスクロール
+        const quizCard = document.getElementsByClassName('quiz-card')[currentQuiz];
+        const quizCardHeight = quizCard.offsetHeight;
+        const position = quizCard.getBoundingClientRect().top + window.pageYOffset - headerHeight - (window.innerHeight - quizCardHeight) / 2;
+        window.scrollTo({ top: position, behavior: 'smooth' });
 
         currentQuiz++;
         setTimeout(displayResult, 3000); // 次の結果を表示するまでの間隔
