c8e763a259b599c43cb71e4ebeb7294b6af94f25
kazuyai <g2172026@tcu.ac.jp>
2023-12-20 11:29:28 +0900

    csvファイルから問題を読み込む仕様に変更

--- script.js
+++ script.js
@@ -1,92 +1,105 @@
-// クイズのデータ
-const quizzes = [
-    {
-        question: "ここに問題1を書く",
-        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
-        correct: "radio1-1"
-    },
-    {
-        question: "ここに問題2を書く",
-        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
-        correct: "radio2-1"
-    },
-    {
-        question: "ここに問題3を書く",
-        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
-        correct: "radio3-1"
-    },
-    {
-        question: "ここに問題4を書く",
-        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
-        correct: "radio4-1"
-    },
-    {
-        question: "ここに問題5を書く",
-        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
-        correct: "radio5-1"
-    },
-    {
-        question: "ここに問題6を書く",
-        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
-        correct: "radio6-1"
-    },
-    {
-        question: "ここに問題7を書く",
-        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
-        correct: "radio7-1"
-    },
-    {
-        question: "ここに問題8を書く",
-        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
-        correct: "radio8-1"
-    },
-    {
-        question: "ここに問題9を書く",
-        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
-        correct: "radio9-1"
-    },
-    {
-        question: "ここに問題10を書く",
-        options: ["選択肢1", "選択肢2", "選択肢3", "選択肢4"],
-        correct: "radio10-1"
+// ロード画面を非表示にする関数
+function hideLoadingScreen() {
+    document.getElementById('loading').style.display = 'none';
+}
+
+let displayedQuizzes = [];
+
+// CSVデータを読み込んでオブジェクトの配列に変換する関数
+// CSVデータを読み込んでオブジェクトの配列に変換する関数
+function convertCSVtoArray(str){
+    var result = [];
+    var tmp = str.split("\n");
+
+    // 最初の行（ヘッダー）を除外してループ
+    for(var i = 1; i < tmp.length; ++i){
+        if (tmp[i].trim() === '') continue; // 空の行は無視する
+
+        var row = tmp[i].split(',');
+
+        // CSVの各行をオブジェクトに変換
+        var quiz = {
+            question: row[0],
+            options: row.slice(1, -1), // 最後の要素（正解のインデックス）を除く
+            correct: `radio${i}-${row[row.length - 1]}` // 正解のIDを生成
+        };
+        result.push(quiz);
     }
-    // 同様に他の問題を追加
-];
+    return result;
+}
 
-function generateQuizzes() {
-    const quizBox = document.querySelector('.quiz-box');
+// CSVファイルを読み込む関数
+function getCSV(){
+    return new Promise(function(resolve, reject){
+        var req = new XMLHttpRequest();
+        req.open("get", "./quizzes.csv", true);
+        req.send(null);
 
-    quizzes.forEach((quiz, quizIndex) => {
-        const quizCard = document.createElement('div');
-quizCard.className = 'quiz-card';
-quizCard.innerHTML = `
-    <p class="quiz-num"><span class="big">${quizIndex + 1}</span>問目</p>
-    <p class="quiz-sentence">${quiz.question}</p>
-    <div class="answer-btn-box">
-        ${quiz.options.reduce((html, option, optIndex) => {
-            // 新しい行を開始するために、インデックスが偶数の時に開始タグを追加
-            if (optIndex % 2 === 0) html += '<div class="btn-row">';
+        req.onload = function(){
+            resolve(convertCSVtoArray(req.responseText));
+        };
 
-            html += `
-                <label>
-                    <input type="radio" name="radio${quizIndex + 1}" id="radio${quizIndex + 1}-${optIndex + 1}">
-                    <span class="radio-button">${option}</span>
-                </label>
-            `;
-
-            // 行を終了するために、インデックスが奇数または最後の要素の時に終了タグを追加
-            if (optIndex % 2 === 1 || optIndex === quiz.options.length - 1) html += '</div>';
-
-            return html;
-        }, '')}
-    </div>
-`;
-quizBox.appendChild(quizCard);
-
+        req.onerror = function() {
+            reject(new Error("CSVのロードに失敗しました"));
+        };
     });
 }
 
-document.addEventListener('DOMContentLoaded', generateQuizzes);
+// CSVデータの読み込みと処理を行う関数
+async function loadData() {
+    try {
+        const allQuizzes = await getCSV();
+        const randomQuizzes = pickRandomQuizzes(allQuizzes, 10);
+        generateQuizzes(randomQuizzes);
+        hideLoadingScreen();
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+// ランダムにクイズを選ぶ関数
+function pickRandomQuizzes(quizzes, count) {
+    // 配列をシャッフル
+    const shuffled = quizzes.sort(() => 0.5 - Math.random());
+    // 最初のcount要素を返す
+    return shuffled.slice(0, count);
+}
+
+function generateQuizzes(quizzes) {
+    const quizBox = document.querySelector('.quiz-box');
+
+    displayedQuizzes = quizzes;
+
+    quizzes.forEach((quiz, quizIndex) => {
+        const quizCard = document.createElement('div');
+        quizCard.className = 'quiz-card';
+        quizCard.innerHTML = `
+            <p class="quiz-num"><span class="big">${quizIndex + 1}</span>問目</p>
+            <p class="quiz-sentence">${quiz.question}</p>
+            <div class="answer-btn-box">
+                ${quiz.options.reduce((html, option, optIndex) => {
+                    // 新しい行を開始するために、インデックスが偶数の時に開始タグを追加
+                    if (optIndex % 2 === 0) html += '<div class="btn-row">';
+
+                    html += `
+                        <label>
+                            <input type="radio" name="radio${quizIndex + 1}" id="radio${quizIndex + 1}-${optIndex + 1}">
+                            <span class="radio-button">${option}</span>
+                        </label>
+                    `;
+
+                    // 行を終了するために、インデックスが奇数または最後の要素の時に終了タグを追加
+                    if (optIndex % 2 === 1 || optIndex === quiz.options.length - 1) html += '</div>';
+
+                    return html;
+                }, '')}
+            </div>
+        `;
+        quizBox.appendChild(quizCard);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', loadData);
 
 function showResults() {
     let currentQuiz = 0;
@@ -95,7 +108,7 @@ function showResults() {
     resultsContainer.innerHTML = ''; // 既存の結果をクリア
 
     function displayResult() {
-        if (currentQuiz >= quizzes.length) {
+        if (currentQuiz >= displayedQuizzes.length) {
             return; // すべてのクイズが完了したら終了
         }
 
