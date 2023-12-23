a925a65846d868c004a63e3a79330e51be4ab4bc
kazuyai <g2172026@tcu.ac.jp>
2023-12-24 01:24:43 +0900

    フェードインに関するスクリプトの追加

--- common.js
+++ common.js
@@ -3,3 +3,15 @@ $(".openbtn").click(function () {
     $("#g-nav").toggleClass('panelactive');
     $(".circle-bg").toggleClass('circleactive');
 });
+
+$(window).scroll(function (){
+	$('.fadein').each(function(){
+		var elemPos = $(this).offset().top,
+		scroll = $(window).scrollTop(),
+		windowHeight = $(window).height();
+
+			if (scroll > elemPos - windowHeight + 150){
+				$(this).addClass('in-view');
+			}
+	});
+});
