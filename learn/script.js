840078e0e4dddd496f674e31bfdb8089d08a8f5e
kazuyai <g2172026@tcu.ac.jp>
2024-01-03 23:44:13 +0900

    テキストタイピングアニメーションの実装

---
+++ learn/script.js
@@ -0,0 +1,44 @@
+function TextTypingAnime() {
+	$('.TextTyping').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			var thisChild = $(this).children();
+			thisChild.each(function (i) {
+				var time = 100;
+				$(this).delay(time * i).fadeIn(time, function() {
+                    if (i === thisChild.length - 1) {
+                        $(this).parent().addClass("completed");
+                    }
+                });
+			});
+		}
+	});
+}
+
+
+// 画面スクロール時の処理
+$(window).scroll(function () {
+	TextTypingAnime();
+});
+
+// 画面読み込み時の処理
+$(window).on('load', function () {
+	var element = $(".TextTyping");
+	element.each(function () {
+		var text = $(this).html();
+		var textbox = "";
+		text.split('').forEach(function (t) {
+			if (t !== " ") {
+				textbox += '<span>' + t + '</span>';
+			} else {
+				textbox += t;
+			}
+		});
+		$(this).html(textbox);
+
+	});
+
+	TextTypingAnime();
+});
