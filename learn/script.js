33114623b8ca320f2159c5ebab9143235bf35bf6
kazuyai <g2172026@tcu.ac.jp>
2024-01-05 02:40:13 +0900

    section間の移動アニメーションの実装

--- learn/script.js
+++ learn/script.js
@@ -6,7 +6,7 @@ function TextTypingAnime() {
 		if (scroll >= elemPos - windowHeight) {
 			var thisChild = $(this).children();
 			thisChild.each(function (i) {
-				var time = 100;
+				var time = 150;
 				$(this).delay(time * i).fadeIn(time, function() {
                     if (i === thisChild.length - 1) {
                         $(this).parent().addClass("completed");
@@ -41,4 +41,65 @@ $(window).on('load', function () {
 	});
 
 	TextTypingAnime();
+});
+
+/*
+$(document).ready(function() {
+    var sections = $('section');
+    var offsets = sections.map(function() {
+        return $(this).offset().top;
+    }).get();
+
+    var isScrolling;
+
+    $(window).on('scroll', function() {
+        window.clearTimeout(isScrolling);
+
+        isScrolling = setTimeout(function() {
+            var scrollTop = $(window).scrollTop();
+
+            var closestSectionIndex = offsets.reduce(function(closestIndex, currentOffset, currentIndex) {
+                return Math.abs(currentOffset - scrollTop) < Math.abs(offsets[closestIndex] - scrollTop) ? currentIndex : closestIndex;
+            }, 0);
+
+            var closestSectionOffset = offsets[closestSectionIndex];
+
+            $('html, body').animate({
+                scrollTop: closestSectionOffset
+            }, 300);
+
+        }, 66);
+    });
+});
+*/
+$(document).ready(function() {
+    var sections = $('section');
+    var offsets = sections.map(function() {
+        return $(this).offset().top;
+    }).get();
+
+    var isScrolling;
+
+    $(window).on('scroll', function() {
+        window.clearTimeout(isScrolling);
+
+        isScrolling = setTimeout(function() {
+            var scrollTop = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            var middleOfScreen = scrollTop + windowHeight / 2;
+
+            var closestSectionIndex = offsets.findIndex(function(offset, index) {
+                var nextOffset = index + 1 < offsets.length ? offsets[index + 1] : Infinity;
+                return middleOfScreen >= offset && middleOfScreen < nextOffset;
+            });
+
+            if (closestSectionIndex !== -1) {
+                var closestSectionOffset = offsets[closestSectionIndex];
+                $('html, body').animate({
+                    scrollTop: closestSectionOffset
+                }, 300);
+            }
+
+        }, 66);
+    });
 });
